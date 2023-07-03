5c97f31888ca5923ef0edaaf535d0a6fba842f8c
vetriashwath <ashwathv20@gmail.com>
2023-07-03 11:26:13 +0530

    join and remove member

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import discord
 from discord.ext import commands
 
+#api tokens and bot token
+from apikeys import *
+
 intents = discord.Intents.all()
+intents.members= True
+
 client = commands.Bot(command_prefix = '$',intents=intents)
 #client = commands.Bot(command_prefix="$")
 
@@ -14,5 +19,16 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send("Hey, I am fern")
 
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1124620362366320681)
+    await channel.send("hey")
 
-client.run('MTEyNDk4NjAxMjc3MTMwNzU5MA.G7O2_A.cQ4RwIpua1xmh0u-RkWv-PDJZloGN57Lxd8d0A')
+@client.event    
+async def on_member_remove(member):
+    channel = client.get_channel(1124620362366320681)
+    await channel.send("f off")
+
+
+
+client.run(bottoken)
